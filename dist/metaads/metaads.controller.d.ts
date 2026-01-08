62c96dce0dbe90548d1828fc7c19c4eced369b29
yunior <yunior>
2026-01-07 20:29:02 -0400

    chore: limpieza técnica y configuración correcta de gitignore

--- dist/metaads/metaads.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { MetaadsService } from './metaads.service';
-export declare class MetaadsController {
-    private readonly metaadsService;
-    constructor(metaadsService: MetaadsService);
-    getAdAccounts(accessToken: string): Promise<any>;
-    getInsights(adAccountId: string, datepreset: string, accessToken: string): Promise<any>;
-}
